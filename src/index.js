d4ba859b236cb67cdf4f2d0615e3062b44a0e267
pong34811 <pong34811@gmail.com>
2024-12-21 21:47:13 +0700

    update vercel Analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./srceen/home/Home";
+import { Analytics } from '@vercel/analytics/next';
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Analytics />
       </Routes>
     </BrowserRouter>
   );
