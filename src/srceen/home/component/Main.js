effbf38080dfbece5533476fd6dbf31629f24da4
pong34811 <pong34811@gmail.com>
2024-12-25 22:22:40 +0700

    fix UX/UI Main.js and Main.css

--- src/srceen/home/component/Main.js
+++ src/srceen/home/component/Main.js
@@ -13,59 +13,57 @@ function Main() {
 
   return (
     <div className="container-main-home" style={backgroundImage}>
-      <div className="container-main-home-text">
-        <div className="">
-          <div>
-            <h2>
-              Intro<span>duction</span>
-            </h2>
+      <div className="main-col">
+        <div className="h2-introduction">
+          <h2>
+            Intro<span>duction</span>
+          </h2>
+        </div>
+        <div className="h4-nice-to-meet-you">
+          <h4>Nice to meet you, Hello</h4>
+          <h1>
+            My name is <span>Kayna</span>
+          </h1>
+          <h3>I'am Vtuber Thailand</h3>
+          <div className="social-media">
+          <Link
+            to={"https://www.youtube.com/@KaynaVtuberTH"}
+            className="btn-youtube"
+            target="_blank"
+
+          >
+            Youtube
+          </Link>
+          <Link
+            to={"https://www.tiktok.com/@kayna_channel"}
+            className="btn-tiktik"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tiktok
+          </Link>
+          <Link
+            to={"https://www.facebook.com/KaynaVTB"}
+            className="btn-facebook"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </Link>
+        </div>
+        </div>
+        <div className="status-youtube">
+          <div className="youtube-follow">
+            <h2>Follow</h2>
+            <span>2000</span>
           </div>
-          <div>
-            <h4>Nice to meet you, Hello</h4>
-            <h1>
-              My name is <span>Kayna</span>
-            </h1>
-            <h3>I'am a Ghoul :D.</h3>
+          <div className="youtube-video">
+            <h2>Video</h2>
+            <span>500</span>
           </div>
-          <div>
-            <a
-              href="https://www.youtube.com/@KaynaVtuberTH"
-              className="btn-youtube"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Youtube
-            </a>
-            <a
-              href="https://www.tiktok.com/@kayna_channel"
-              className="btn-tiktik"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Tiktok
-            </a>
-            <a
-              href="https://www.facebook.com/KaynaVTB"
-              className="btn-facebook"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Facebook
-            </a>
-          </div>
-          <div className="">
-            <div>
-              <h2>Follow</h2>
-              <span>2000</span>
-            </div>
-            <div>
-              <h2>Video</h2>
-              <span>500</span>
-            </div>
-            <div>
-              <h2>View</h2>
-              <span>10000000</span>
-            </div>
+          <div className="youtube-view">
+            <h2>View</h2>
+            <span>10000000</span>
           </div>
         </div>
       </div>
