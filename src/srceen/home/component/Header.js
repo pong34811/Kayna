8ce54e799ec0e1bc458f3c09a2f44932bc41cc08
pong34811 <pong34811@gmail.com>
2024-12-28 21:30:57 +0700

    add Footer and Header components with corresponding styles, update Introduction.js and About.css for improved layout and responsiveness

--- src/srceen/home/component/Header.js
+++ src/srceen/home/component/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav className="nav">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/commission">Commission</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/project">Project</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
-export default Header
+export default Header;
