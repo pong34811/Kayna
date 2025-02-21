fd86f41b3fae3ac0e104441a558fe6c041922b38
Kanya Lenovo <pong34811@gmail.com>
2025-02-21 13:49:43 +0700

    feat: add @vercel/analytics for enhanced performance tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
+import { Analytics } from '@vercel/analytics/next';
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
+  <Analytics />
 }
